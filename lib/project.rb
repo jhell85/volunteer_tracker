12f207844c5466e788b39c402314c80cba1f9dbc
jhell85 <jhell85@gmail.com>
2020-05-22 11:28:21 -0700

    add initialize method to get first 2 tests of project_spec passing

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,13 @@
+require 'pry'
+
 class Project 
-attr_accessor :title, :id, 
+attr_accessor :title, :id
 
-def initialize(attributes)
-  @id = attributes.fetch(:id)
-  @title = attributes.fetch(:title)
+  def initialize(attributes)
+    @id = attributes.fetch(:id)
+    @title = attributes.fetch(:title)
+  end
+  # def ==(project_to_compare)
+  #   self.title == project_to_compare.title && self.id == project_to_compare.id
+  # end
 end
-
