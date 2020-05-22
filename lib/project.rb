3448c6a4a90b0f0ef0705ef38d828ab82602fceb
jhell85 <jhell85@gmail.com>
2020-05-22 11:37:21 -0700

    add save method to project class

--- lib/project.rb
+++ lib/project.rb
@@ -7,7 +7,13 @@ attr_accessor :title, :id
     @id = attributes.fetch(:id)
     @title = attributes.fetch(:title)
   end
-  # def ==(project_to_compare)
-  #   self.title == project_to_compare.title && self.id == project_to_compare.id
-  # end
+
+  def save 
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch('id').to_i
+  end
+
+  def ==(project_to_compare)
+    self.title == project_to_compare.title && self.id == project_to_compare.id
+  end
 end
