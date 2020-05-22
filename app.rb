62399b5e68021082dbde671327334703b92b0cbb
jhell85 <jhell85@gmail.com>
2020-05-22 16:33:31 -0700

    added routes for home page

---
+++ app.rb
@@ -0,0 +1,18 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+require('pg')
+also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects') do
+  redirect to('/')
+end
