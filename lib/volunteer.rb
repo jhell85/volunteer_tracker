5435c3a11b517584284d69785df12db3d2d45c8d
jhell85 <jhell85@gmail.com>
2020-05-22 14:55:25 -0700

    add find method to volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -26,4 +26,13 @@ class Volunteer
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
     @id = result.first.fetch("id").to_i
   end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    name = volunteer.fetch("name")
+    id = volunteer.fetch("id").to_i
+    project_id = volunteer.fetch("project_id").to_i
+    Volunteer.new({name: name, id: id, project_id: project_id})
+  end
+    
 end
