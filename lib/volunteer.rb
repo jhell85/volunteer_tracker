92c3a3f354cb10da418bba0b7fd5c27041243790
jhell85 <jhell85@gmail.com>
2020-05-22 14:32:05 -0700

    add .all and #save method to volunteer

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -5,4 +5,25 @@ class Volunteer
     @id = attributes.fetch(:id)
     @project_id = attributes.fetch(:project_id)
   end
+
+  def ==(volunteer_to_compare)
+    self.name == volunteer_to_compare.name && self.id == volunteer_to_compare.id
+  end
+
+  def self.all 
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each  do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      project_id = volunteer.fetch("project_id").to_i
+      volunteers.push(Volunteer.new({name: name, id: id, project_id: project_id}))
+    end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
 end
