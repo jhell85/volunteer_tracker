4ce3686bf524862e64605a818a01bf34874074b2
jhell85 <jhell85@gmail.com>
2020-05-22 11:54:48 -0700

    add .all method to project to get test 5 and 6 passing

--- lib/project.rb
+++ lib/project.rb
@@ -16,4 +16,15 @@ attr_accessor :title, :id
   def ==(project_to_compare)
     self.title == project_to_compare.title && self.id == project_to_compare.id
   end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({title: title, id: id}))
+    end
+    projects
+  end
 end
