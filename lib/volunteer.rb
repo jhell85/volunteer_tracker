16626584c7e86b61f263936167373c4bfda17cef
jhell85 <jhell85@gmail.com>
2020-05-22 10:55:43 -0700

    add volunteer class

---
+++ lib/volunteer.rb
@@ -0,0 +1,8 @@
+class Volunteer
+   attr_accessor :name, :id, :project_id
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
+  end
+end
