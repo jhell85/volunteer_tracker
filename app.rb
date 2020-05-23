a95355ebe3eab2792e19443d5a901fabda4a4853
jhell85 <jhell85@gmail.com>
2020-05-22 18:42:13 -0700

    added routes for the home page and post route for adding a project

--- app.rb
+++ app.rb
@@ -9,10 +9,18 @@ also_reload('lib/**/*.rb')
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get('/') do
+  # DB.exec('DELETE FROM projects *;')
   @projects = Project.all
   erb(:projects)
 end
 
 get('/projects') do
+  redirect to('/')
+end
+
+post('/projects') do 
+  title = params[:title]
+  project = Project.new({title: title, id: nil})
+  project.save
   redirect to('/')
 end
