74246747e7b4a91987d47b021c525f071cec7908
jhell85 <jhell85@gmail.com>
2020-05-24 09:08:16 -0700

    added routes for posting volunteers and going to edit pages

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ also_reload('lib/**/*.rb')
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get('/') do
-  # DB.exec('DELETE FROM projects *;')
   @projects = Project.all
   erb(:projects)
 end
@@ -23,4 +22,47 @@ post('/projects') do
   project = Project.new({title: title, id: nil})
   project.save
   redirect to('/')
+end
+
+get('/projects/:id') do 
+  @project = Project.find(params[:id].to_i)
+  erb(:project_details)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i)
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do 
+  project = Project.find(params[:id].to_i)
+  project.update({title: params[:title]})
+  redirect to('/')
+end
+
+delete('/projects/:id') do 
+  project = Project.find(params[:id].to_i)
+  project.delete
+  redirect to('/')
+end
+
+post('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i)
+  volunteer = Volunteer.new({name: params[:name], id: nil, project_id: @project.id})
+  volunteer.save
+  erb(:project_details)
+end
+
+get('/volunteers/:id/edit/project/:project_id') do
+  @volunteer = Volunteer.find(params[:id].to_i)
+  @project = Project.find(params[:project_id].to_i)
+  erb(:volunteer_edit)
+end
+
+patch('/volunteers/:id/:project_id') do
+  @volunteer = Volunteer.find(params[:id].to_i)
+  name = params[:name]
+  @volunteer.update({name: name})
+  @project = Project.find(params[:project_id].to_i)
+  erb(:project_details)
 end
